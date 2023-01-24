8e39f5d9cc5bb69505b8f253428daa34ec337fa3
Yan Bisyukov <y.bisyukov@syberry.com>
2023-01-24 08:14:08 +0300

    556433: Implement Book management feature

---
+++ src/main/java/com/syberry/librarymanagementsystem/controller/BookController.java
@@ -0,0 +1,100 @@
+package com.syberry.librarymanagementsystem.controller;
+
+import java.util.List;
+
+import com.syberry.librarymanagementsystem.entity.Book;
+import com.syberry.librarymanagementsystem.service.AuthorService;
+import com.syberry.librarymanagementsystem.service.BookService;
+import com.syberry.librarymanagementsystem.service.CategoryService;
+import com.syberry.librarymanagementsystem.service.PublisherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class BookController {
+
+	private final BookService bookService;
+	private final AuthorService authorService;
+	private final CategoryService categoryService;
+	private final PublisherService publisherService;
+
+	@RequestMapping("/books")
+	public String findAllBooks(Model model) {
+		final List<Book> books = bookService.findAllBooks();
+
+		model.addAttribute("books", books);
+		return "list-books";
+	}
+
+	@RequestMapping("/searchBook")
+	public String searchBook(@Param("keyword") String keyword, Model model) {
+		final List<Book> books = bookService.searchBooks(keyword);
+
+		model.addAttribute("books", books);
+		model.addAttribute("keyword", keyword);
+		return "list-books";
+	}
+
+	@RequestMapping("/book/{id}")
+	public String findBookById(@PathVariable("id") Long id, Model model) {
+		final Book book = bookService.findBookById(id);
+
+		model.addAttribute("book", book);
+		return "list-book";
+	}
+
+	@GetMapping("/add")
+	public String showCreateForm(Book book, Model model) {
+		model.addAttribute("categories", categoryService.findAllCategories());
+		model.addAttribute("authors", authorService.findAllAuthors());
+		model.addAttribute("publishers", publisherService.findAllPublishers());
+		return "add-book";
+	}
+
+	@RequestMapping("/add-book")
+	public String createBook(Book book, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "add-book";
+		}
+
+		bookService.createBook(book);
+		model.addAttribute("book", bookService.findAllBooks());
+		return "redirect:/books";
+	}
+
+	@GetMapping("/update/{id}")
+	public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+		final Book book = bookService.findBookById(id);
+
+		model.addAttribute("book", book);
+		return "update-book";
+	}
+
+	@RequestMapping("/update-book/{id}")
+	public String updateBook(@PathVariable("id") Long id, Book book, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			book.setId(id);
+			return "update-book";
+		}
+
+		bookService.updateBook(book);
+		model.addAttribute("book", bookService.findAllBooks());
+		return "redirect:/books";
+	}
+
+	@RequestMapping("/remove-book/{id}")
+	public String deleteBook(@PathVariable("id") Long id, Model model) {
+		bookService.deleteBook(id);
+
+		model.addAttribute("book", bookService.findAllBooks());
+		return "redirect:/books";
+	}
+
+}

4b573d3677c96c446880c618556c07748c5c1343
Yan Bisyukov <y.bisyukov@syberry.com>
2023-01-24 07:59:25 +0300

    536295: Implement authorization feature

---
+++ src/main/java/com/syberry/librarymanagementsystem/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.syberry.librarymanagementsystem.repository;
+
+import com.syberry.librarymanagementsystem.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+	User findByEmail(String email);
+}
